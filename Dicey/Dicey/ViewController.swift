c1862edde044c7f1ae8a1a0b1ca6004bce1291f3
Kooks <hiram.aguirre@gmail.com>
2017-06-16 22:51:09 -0700

    First Commit

---
+++ Dicey/Dicey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dicey
+//
+//  Created by Hiram Aguirre on 6/16/17.
+//  Copyright © 2017 Hiram Aguirre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
